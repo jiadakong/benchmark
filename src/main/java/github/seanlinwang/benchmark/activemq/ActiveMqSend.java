1dc04875ccca401306a3aae04942062d1a38c243
seanlinwang <xalinx@gmail.com>
2011-07-11 18:50:51 +0800

    modify activemqsend

--- src/main/java/github/seanlinwang/benchmark/activemq/ActiveMqSend.java
+++ src/main/java/github/seanlinwang/benchmark/activemq/ActiveMqSend.java
@@ -1,8 +1,8 @@
 package github.seanlinwang.benchmark.activemq;
 
+import github.seanlinwang.benchmark.BenchmarkBase;
 import github.seanlinwang.benchmark.util.Format;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.DateFormat;
@@ -24,15 +24,17 @@ import org.springframework.jms.core.MessageCreator;
 /**
  * mq sender
  */
-public class MySender implements Runnable {
+public class ActiveMqSend extends BenchmarkBase {
 
 	private JmsTemplate template;
 	private Destination destination;
 	private String[] resultSet;
 	private DateFormat dateFormat = Format.getDateFormat();
 	private Calendar cal = Format.getCalendar();
+	private PrintWriter out;
 
-	public MySender(int times) {
+	public ActiveMqSend(PrintWriter out, int times) {
+		this.out = out;
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("/applicationContext.xml");
 		template = (JmsTemplate) ctx.getBean("jmsTemplate");
 		template.setDeliveryMode(DeliveryMode.PERSISTENT);
@@ -60,9 +62,11 @@ public class MySender implements Runnable {
 	}
 
 	public static void main(String[] args) throws JMSException, IOException, InterruptedException {
-		MySender[] senders = new MySender[5];
+		PrintWriter out = BenchmarkBase.createOutput("activemq-send.csv");
+		
+		ActiveMqSend[] senders = new ActiveMqSend[5];
 		for (int i = 0; i < senders.length; i++) {
-			senders[i] = new MySender(10000);
+			senders[i] = new ActiveMqSend(out, 10000);
 		}
 		Thread[] ts = new Thread[senders.length];
 		for (int i = 0; i < senders.length; i++) {
@@ -75,25 +79,32 @@ public class MySender implements Runnable {
 			ts[i].join();
 		}
 
-		FileWriter outFile = new FileWriter("/Users/alin/Documents/workspace/logs/benchmark/activemq.csv");
-		PrintWriter out = new PrintWriter(outFile);
 
 		for (int i = 0; i < senders.length; i++) {
-			MySender sender = senders[i];
-			sender.print(out);
+			ActiveMqSend sender = senders[i];
+			sender.print();
 		}
 
 		out.close();
 
 	}
 
-	private void print(PrintWriter out2) {
+	private void print() {
 		for (String line : resultSet) {
 			String[] segs = StringUtils.split(line, ',');
-			out2.write(segs[0]);
-			out2.write(",");
+			out.write(segs[0]);
+			out.write(",");
 			this.cal.setTimeInMillis(Long.valueOf(segs[1]));
-			out2.write(this.dateFormat.format(cal.getTime()) + "\n");
+			out.write(this.dateFormat.format(cal.getTime()) + "\n");
 		}
+	}
+
+	/* (non-Javadoc)
+	 * @see github.seanlinwang.benchmark.BenchmarkBase#deleteTestData()
+	 */
+	@Override
+	protected void deleteTestData() {
+		// TODO Auto-generated method stub
+		
 	}
 }
